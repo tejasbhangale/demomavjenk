2f3db63d40e0bf695b3d7bb1d4a9b2997256b031
Bhangale <tejas.bhangale@capgemini.com>
2018-10-01 12:18:37 +0530

    1 class added

---
+++ quickstart/src/main/java/com/cg/quickstart/Company.java
@@ -0,0 +1,17 @@
+package com.cg.quickstart;
+
+public class Company {
+	private String name="Capgemini";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
